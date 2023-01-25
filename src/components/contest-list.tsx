4155fb4f5993e209f06d8fb45367072197e2ce49
rameshlakmal <rameshslakmal1999@gmail.com>
2023-01-25 17:22:48 +0530

    Fetching data while react is rendering

--- src/components/contest-list.tsx
+++ src/components/contest-list.tsx
@@ -1,10 +1,20 @@
 import ContestPreview from "./contest-preview";
+import {useEffect,useState} from "react";
+import { fetchContests } from "../api-client";
 
-const ContestList = ({ contests }) => {
+const ContestList = ({ initialContests }) => {
+  const [contests,setContests] = useState(initialContests);
+  
+  useEffect(()=>{
+    fetchContests().then((data) => {
+      setContests(data.contests);
+    });
+  },[]);
+   
   return (
     <div className="contest-list">
       {contests.map((contest) => {
-        return <ContestPreview contest={contest} />;
+        return <ContestPreview key={contest.id} contest={contest} />;
       })}
     </div>
   );
